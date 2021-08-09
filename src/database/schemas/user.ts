2b9915e1718ca1a024b3b196ea3ad09d1581e73f
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-09 11:24:52 -0300

    feat: add get method for user Route

--- src/database/schemas/user.ts
+++ src/database/schemas/user.ts
@@ -8,7 +8,7 @@ import {
 } from "typeorm";
 
 @Entity()
-export class User {
+class User {
   @ObjectIdColumn()
   _id: ObjectID;
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,16 +1,22 @@
-import { Router } from "express"
-import { getMongoRepository } from 'typeorm';
-import {User} from '../database/schemas/user';
+import { Router, Request, Response } from "express";
+import { getMongoRepository } from "typeorm";
+import User from "../database/schemas/user";
 
+const userRouter = Router();
 
-const userRouter = Router()
-userRouter.post('/api/user', async (req, res) => {
-    const repository = getMongoRepository(User);
-
-    const userToSave = repository.create(req.body);
-    const user = await repository.save(userToSave);
-
-    return res.status(201).json(user);
+userRouter.get("/api/user", async (req: Request, res: Response) => {
+  const repository = getMongoRepository(User);
+  const returnGet = await repository.find();
+  return res.json(returnGet);
 });
 
-export {userRouter}
+userRouter.post("/api/user", async (req: Request, res: Response) => {
+  const repository = getMongoRepository(User);
+
+  const userToSave = repository.create(req.body);
+  const user = await repository.save(userToSave);
+
+  return res.status(201).json(user);
+});
+
+export { userRouter };
