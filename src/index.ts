3f7d5d390f955f5978da8ae0d3efa440d77ddd0e
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-11 22:28:58 -0300

    feat: add companyRouter in Server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import express from "express";
 import "./database/connection";
 import {userRouter}  from "./routes/user";
+import {companyRouter}  from "./routes/company";
+
 
 const app = express();
 app.use(express.json());
@@ -10,4 +12,4 @@ app.listen(3333, () => {
   console.log("Server is running in port 3333");
 });
 
-app.use(userRouter);
+app.use(userRouter, companyRouter);
