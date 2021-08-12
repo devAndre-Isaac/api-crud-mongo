b4be6682dd3443383f87765a49965512d44b0fe3
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-11 22:28:27 -0300

    feat: add post method for company route

--- src/routes/company.ts
+++ src/routes/company.ts
@@ -5,9 +5,19 @@ import { getValidData } from "../validation/validatorHandle";
 
 const companyRouter = Router();
 
-companyRouter.get("/api/user", async (req: Request, res: Response) => {
-    const repository = getMongoRepository(Company);
-    const companyToRead = await repository.find();
-    return res.json(companyToRead);  
+companyRouter.get("/api/company", async (req: Request, res: Response) => {
+  const repository = getMongoRepository(Company);
+  const companyToRead = await repository.find();
+  return res.json(companyToRead);
 });
-  
+
+companyRouter.post("/api/company", async (req: Request, res: Response) => {
+  const repository = getMongoRepository(Company);
+  const companyToCreate =  repository.create(req.body);
+  const company = await repository.save(companyToCreate);
+  return res.status(201).json(company);
+});
+
+
+
+export {companyRouter}
