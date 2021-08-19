ef07cc25f872fa180eefe5d4ec59f161e236064b
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-19 19:45:53 -0300

    style: :art: add message for connect with database

--- src/database/connection/index.ts
+++ src/database/connection/index.ts
@@ -1,3 +1,5 @@
-import { createConnection } from 'typeorm';
+import { createConnection } from "typeorm";
 
-createConnection();
+createConnection().then(() =>
+  console.log("📦 Successfully connected with database")
+);
