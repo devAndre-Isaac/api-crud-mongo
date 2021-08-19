17e61a93b7b73dbf2ccd83abb61d2f309bc174af
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-19 19:45:00 -0300

    feat: :art: formating server started

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import "reflect-metadata";
 import express from "express";
 import "./database/connection";
-import {userRouter}  from "./routes/user";
-import {companyRouter}  from "./routes/company";
-
+import { userRouter } from "./routes/user";
+import { companyRouter } from "./routes/company";
 
 const app = express();
 app.use(express.json());
+app.use(userRouter, companyRouter);
 
 app.listen(3333, () => {
-  console.log("Server is running in port 3333");
+  console.log("⚡Server started at http://localhost:3333");
 });
-
-app.use(userRouter, companyRouter);
