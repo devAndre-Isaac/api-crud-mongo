75e4b80ef718cfe40226870cf7ba449f50f53ff1
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-21 18:29:26 -0300

    feat: :sparkles: add validation for company exists

--- src/routes/company.ts
+++ src/routes/company.ts
@@ -17,6 +17,11 @@ companyRouter.get("/api/company", async (req: Request, res: Response) => {
 
 companyRouter.post("/api/company", async (req: Request, res: Response) => {
   const repository = getMongoRepository(Company);
+  const { cnpj } = req.body;
+  const companyExists = await repository.findOne({ where: { cnpj } });
+  if (companyExists) {
+    return res.sendStatus(409);
+  }
   const companyToCreate = repository.create(req.body);
   const company = await repository.save(companyToCreate);
   return res.status(201).json(company);
