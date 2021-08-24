03e872b5a033bb8c57ef3a0409edeeb09798a9ad
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-24 10:48:37 -0300

    refactor: :recycle: add controller for  create method

--- src/controllers/CompanyControlller.ts
+++ src/controllers/CompanyControlller.ts
@@ -13,6 +13,17 @@ class CompanyController {
     const companyToRead = await repository.findAndCount();
     return res.json(companyToRead);
   }
+  async store(req: Request, res: Response){
+    const repository = getMongoRepository(Company);
+    const { cnpj } = req.body;
+    const companyExists = await repository.findOne({ where: { cnpj } });
+    if (companyExists) {
+      return res.sendStatus(409);
+    }
+    const companyToCreate = repository.create(req.body);
+    const company = await repository.save(companyToCreate);
+    return res.status(201).json(company);
+  }
 }
 
 export default new CompanyController();
--- src/routes/company.ts
+++ src/routes/company.ts
@@ -8,24 +8,7 @@ const companyRouter = Router();
 
 companyRouter.get("/api/company", CompanyControlller.read);
 
-companyRouter.post("/api/company", async (req: Request, res: Response) => {
-  const repository = getMongoRepository(Company);
-  const { cnpj } = req.body;
-  const companyExists = await repository.findOne({ where: { cnpj } });
-  if (companyExists) {
-    return res.sendStatus(409);
-  }
-  const companyToCreate = repository.create(req.body);
-  const company = await repository.save(companyToCreate);
-  return res.status(201).json(company);
-});
+companyRouter.post("/api/company", CompanyControlller.store);
 
-companyRouter.delete("/api/user/:_id", async (req: Request, res: Response) => {
-  const repository = getMongoRepository(Company);
-  const { params } = getValidData(req);
-  const companyToRemove = repository.delete(params.id);
-  const company = repository.deleteOne(companyToRemove);
-  return res.status(204).json(company);
-});
 
 export { companyRouter };
