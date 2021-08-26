7766365f5fb17a95f4f3fc2314f07019776b2636
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-26 19:37:52 -0300

    feat: :sparkles: add route for authCompany

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,5 +3,8 @@ import AuthController from "../controllers/AuthController";
 const authRouter = Router();
 
 authRouter.post("/auth", AuthController.authUser);
+authRouter.post("/auth", AuthController.authCompany);
+
+
 
 export default authRouter;
