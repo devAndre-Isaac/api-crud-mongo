09d805cbee07d52e070f4cf23c7ed6b452c232a7
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:10:38 -0300

    refactor: :recycle: remove unused route  for user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,8 +3,6 @@ import UserController from "../controllers/UserController";
 
 const userRouter = Router();
 
-userRouter.get("/read/user", UserController.read);
-
 userRouter.post("/create/user", UserController.store);
 
 userRouter.delete("/remove/user/:_id", UserController.remove);
