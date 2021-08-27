4020b68fe6d9a8621ce4e792794237c4853e079b
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:12:44 -0300

    feat: :sparkles: add route for admin read company

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,5 +4,6 @@ import AdminController from "../controllers/AdminController";
 const adminRouter = Router();
 
 adminRouter.get("/admin/read/user", AdminController.readUser);
+adminRouter.get("/admin/read/company", AdminController.readCompany);
 
 export { adminRouter };
