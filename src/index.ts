34673a40a84b6a8a94a415111cb899caa2748e7f
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:06:28 -0300

    feat: :sparkles: add adminRouter

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import "./database/connection";
 import { userRouter } from "./routes/user";
 import { companyRouter } from "./routes/company";
 import authRouter from "./routes/auth";
+import { adminRouter } from "./routes/admin";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(userRouter, companyRouter, authRouter);
+app.use(userRouter, companyRouter, authRouter, adminRouter);
 
 app.listen(3333, () => {
   console.log("⚡Server started at http://localhost:3333");
