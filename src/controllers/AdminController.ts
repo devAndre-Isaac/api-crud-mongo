86e7765e0fafc48667f4049a500dfb77eef36aa4
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:06:08 -0300

    feat: :sparkles: add controller for admin

---
+++ src/controllers/AdminController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { getMongoRepository } from "typeorm";
+import User from "../database/schemas/user";
+
+class AdminController {
+  async readUser(req: Request, res: Response) {
+    const repository = getMongoRepository(User);
+    const userGetAll = await repository.findAndCount();
+    return res.json({users: userGetAll});
+  }
+}
+
+export default new AdminController();
