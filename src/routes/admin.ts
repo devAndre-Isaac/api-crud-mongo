85c9d4c876edee780bb0f06dc17965468f8928f8
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:05:25 -0300

    feat: :sparkles: add admin route

---
+++ src/routes/admin.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import AdminController from "../controllers/AdminController";
+
+
+const adminRouter = Router()
+
+adminRouter.get("/admin/read/user", AdminController.readUser);
+
+export { adminRouter };
