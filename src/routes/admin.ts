98af670f6119377a572eb5fdc139924b4c3700d8
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:13:11 -0300

    style: :art: format code

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,6 +4,7 @@ import AdminController from "../controllers/AdminController";
 const adminRouter = Router();
 
 adminRouter.get("/admin/read/user", AdminController.readUser);
+
 adminRouter.get("/admin/read/company", AdminController.readCompany);
 
 export { adminRouter };
