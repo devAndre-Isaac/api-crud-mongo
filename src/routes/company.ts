e69b83e65fd1a6fcfb003b39eb3fa2d5d4e72dd9
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-27 14:05:53 -0300

    refactor: :recycle: remove getall route in company

--- src/routes/company.ts
+++ src/routes/company.ts
@@ -3,8 +3,6 @@ import CompanyControlller from "../controllers/CompanyControlller";
 
 const companyRouter = Router();
 
-companyRouter.get("/read/company", CompanyControlller.read);
-
 companyRouter.post("/create/company", CompanyControlller.store);
 
 companyRouter.delete("/remove/user/:_id", CompanyControlller.remove);
