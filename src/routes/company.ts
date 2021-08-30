6fe826b69bbffdd8f74b5a84a4c8eb0daa10396d
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-30 19:06:46 -0300

    feat: :sparkles: insert validation in company route

--- src/routes/company.ts
+++ src/routes/company.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import CompanyControlller from "../controllers/CompanyControlller";
+import { createCompanyValidator } from "../validation/companyValidator";
 
 const companyRouter = Router();
 
-companyRouter.post("/api/create/company", CompanyControlller.store);
+companyRouter.post(
+  "/api/create/company",
+  createCompanyValidator,
+  CompanyControlller.store
+);
 
 companyRouter.delete("/api/remove/user/:_id", CompanyControlller.remove);
 
