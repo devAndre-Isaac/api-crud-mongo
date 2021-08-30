10bae8548c7ab1c0acd0c3cab5eff72bb98499a7
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-30 19:03:28 -0300

    feat: :sparkles: insert validation in user route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import { createUserValidator } from "../validation/userValidator";
 
 const userRouter = Router();
 
-userRouter.post("/api/create/user", UserController.store);
+userRouter.post("/api/create/user", createUserValidator, UserController.store);
 
 userRouter.delete("/api/remove/user/:_id", UserController.remove);
 
