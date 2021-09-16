0cd16b85a0661fdcc07c4b2494478a7ac2a80b4f
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-16 18:21:07 -0300

    feat: :sparkles: insert header for getAll in Company

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -14,9 +14,13 @@ class AdminController {
     return res.json({ users: userGetAll, form });
   }
   async readCompany(req: Request, res: Response) {
+    const { form } = req.headers;
+    if (form === undefined) {
+      return res.send("insert form in header");
+    }
     const repository = getMongoRepository(Company);
     const companyGetAll = await repository.findAndCount();
-    return res.json({ companies: companyGetAll });
+    return res.json({ companies: companyGetAll, form });
   }
 }
 
