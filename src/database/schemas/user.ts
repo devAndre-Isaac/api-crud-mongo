2c033e805350e27a7963511c2c7c4cce1af4c7fc
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-17 20:07:02 -0300

    refactor: :recycle: refactor hash bcrypt

--- src/database/schemas/user.ts
+++ src/database/schemas/user.ts
@@ -29,7 +29,7 @@ class User {
   @BeforeInsert()
   @BeforeUpdate()
   hashPassword() {
-    this.password = bcrypt.hashSync(this.password, 8);
+    this.password = bcrypt.hashSync(this.password, 7);
   }
 
   @CreateDateColumn()
