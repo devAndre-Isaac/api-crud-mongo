91284f2daac988fb9ca8f239d360fc453d6e3b78
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-17 20:08:16 -0300

    feat: :sparkles: add put route for user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,7 +5,7 @@ import { createUserValidator } from "../validation/userValidator";
 const userRouter = Router();
 
 userRouter.post("/api/create/user", createUserValidator, UserController.store);
-
 userRouter.delete("/api/remove/user/:_id", UserController.remove);
+userRouter.put("/api/update/user/:_id", UserController.update);
 
 export { userRouter };
