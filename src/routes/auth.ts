46450af39511dbf62d5cdc855aed17cc326452d4
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-19 18:23:15 -0300

    refactor: :recycle: change file name

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthController from "../controllers/AuthController";
-
-const authRouter = Router();
-
-authRouter.post("/api/user/auth", AuthController.authUser);
-authRouter.post("/api/company/auth", AuthController.authCompany);
-
-export { authRouter };
