32f375b0b92659c78f183535f70fb5c00e84053f
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-19 18:22:02 -0300

    refactor: :recycle: refactor file name

---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import AdminController from "../controllers/AdminController";
+
+const adminRouter = Router();
+
+adminRouter.get("/api/admin/read/user", AdminController.readUser);
+adminRouter.get("/api/admin/read/company", AdminController.readCompany);
+
+export { adminRouter };
