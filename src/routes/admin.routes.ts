7c68ac88307ac44de789c9b85f471bc3f947ffe4
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-20 20:04:40 -0300

    feat: :sparkles: add getById route for user

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,6 +4,8 @@ import AdminController from "../controllers/AdminController";
 const adminRouter = Router();
 
 adminRouter.get("/api/admin/read/user", AdminController.readUser);
+adminRouter.get("/api/admin/read/user/:_id", AdminController.readUserById);
+
 adminRouter.get("/api/admin/read/company", AdminController.readCompany);
 
 export { adminRouter };
