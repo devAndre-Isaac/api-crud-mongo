5c7fd48389d71cb56ea3d5c0f2e11a013d2eecef
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-20 20:04:59 -0300

    feat: :sparkles: add controller for getById User

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getMongoRepository } from "typeorm";
+
 import Company from "../database/schemas/company";
 import User from "../database/schemas/user";
 
@@ -9,6 +10,16 @@ class AdminController {
     const userGetAll = await repository.findAndCount();
     return res.json({ Users: userGetAll });
   }
+  async readUserById(req: Request, res: Response) {
+    const repositoryId = getMongoRepository(User);
+    const { _id } = req.params;
+    const idReturn = await repositoryId.findOne(_id);
+    if (!idReturn) {
+      return res.send({ Message: "Identification does not exist" });
+    }
+    return res.json(idReturn);
+  }
+
   async readCompany(req: Request, res: Response) {
     const repository = getMongoRepository(Company);
     const companyGetAll = await repository.findAndCount();
